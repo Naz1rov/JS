f6ea03a88931e9bb747ada8ac775c43bfc808b2c
ozodbek <nozodbek572@gmail.com>
2024-10-14 14:49:29 +0500

    nazirov

---
+++ main.js
@@ -0,0 +1,64 @@
+// 3-misol boshlanish
+
+// let math = +prompt("matematikadan olgan balingizni kiriting ");
+// let english = +prompt("ingliz tilidan olgan balingizni kiriting");
+// let phsics = +prompt("fizika tilidan olgan balingizni kiriting");
+// let russian = +prompt("rus tilidan olgan balingizni kiriting");
+// let PE = +prompt("jismonniy tilidan olgan balingizni kiriting");
+
+// alert(
+//   `sizning umumiy balingiz ${(math + english + phsics + russian + PE) / 5}`
+// );
+
+// console.log(
+//   `sizning umumiy balingiz ${(math + english + phsics + russian + PE) / 5}`
+// );
+
+// Personal Information Form // kamida 10ta bolishi kk
+
+// Shopping Total Calculator kamida 5ta product (olma behi karoshka...)
+
+// Avarage Grade Calculator kamida 5ta fan(math,phisics,english ...)
+  
+
+// 2-misol
+
+// let names = prompt("ismingizni kiriting ")
+// let country = prompt("shaharingizni kiriting")
+// let age = prompt("yoshingizni kiriting ")
+// let footballClub = prompt("jamoangizni kiriting ")
+// let gender = prompt("jinsangizni kiriting (male/female) ")
+// let surname = prompt("familyangizni kiriting")
+// let yourCarBrand = prompt("mashinangiz brendini kirgizing")
+// let email = prompt("emailingizni kiriting")
+// let password = prompt("parolingizni kiriting")
+
+// console.log(`
+//     Sizing ismingiz: ${names},
+//     Sizning manziligniz: ${country},
+//     Sizning yoshinigiz: ${age}
+//     sizning klubingiz ${footballClub}
+//     sizning jinsingiz ${gender}
+//     sizning famliyangiz ${surname}
+//     sizning mashina brendingiz ${yourCarBrand}
+//     sizning emailingiz ${email}
+//     sizning parolingiz ${password}
+
+
+
+//   `);
+
+// 1-misol 
+
+// let kartoshka = +prompt("kartoshka narxi ");
+// let piyoz = +prompt("piyoz narxi");
+// let sabzi = +prompt("sabzi narsxi");
+// let pamidor = +prompt("pamidor narxi");
+// let bodring = +prompt("bodring narxi");
+
+// alert(
+//   `sizning umumiy narxingiz  ${(kartoshka + piyoz + sabzi + pamidor + bodring) }`
+// );
+
+
+
